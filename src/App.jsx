9cdb0c93c37807ff5f1ee770af0ec142adeb6b14
CodeLearnApp <codelearn.app@gmail.com>
2026-09-24 00:25:48 -0300

    Update API key variable for Judge0 service

--- src/App.jsx
+++ src/App.jsx
@@ -678,7 +678,7 @@ function Playground({ code, progLang, t }) {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "X-RapidAPI-Key": import.meta.env.VITE_JUDGE0_KEY,
+         "X-RapidAPI-Key": import.meta.env.VITE_JUDGE0_API_KEY,
           "X-RapidAPI-Host": "judge0-ce.p.rapidapi.com",
         },
         body: JSON.stringify({
